420f6c430ceec3f6341c7babb2cdaf11adc0f833
admin <lorenznatividad1996@gmail.com>
2023-09-29 18:39:13 +0800

    Updated LinksRetriever - Added functionality -- Get status of each links -- added status of invalid links -- removed links of the same target page -- added multithread to avoid memory leaks

--- src/main/java/com/bibvip/SeleniumRunner.java
+++ src/main/java/com/bibvip/SeleniumRunner.java
@@ -5,19 +5,24 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import lombok.extern.slf4j.Slf4j;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.FileWriter;
+import java.io.IOException;
 import java.io.Writer;
 import java.net.MalformedURLException;
 import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import static com.bibvip.configs.consts.BibEnglishLinks.*;
 import static com.bibvip.utility.AppUtil.getBy;
-import static com.bibvip.utility.LinksRetriever.getAllLinksInPage;
+import static com.bibvip.utility.LinksRetriever.*;
 import static com.bibvip.utility.LocalizationChecker.chineseCheckWordsInPage;
 import static com.bibvip.utility.LocalizationChecker.englishCheckWordsInPage;
 import static com.bibvip.utility.ThinkingTimeUtil.getElementWithPolling;
@@ -28,7 +33,32 @@ import static com.bibvip.variables.LoginVars.PASSWORD_TEXTFIELD;
 public class SeleniumRunner {
 
     public static void main(String[] args) throws Exception {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--start-maximized");
+        WebDriver driver = new ChromeDriver(options);
+        // Open a new window for each URL.
+        String url = "https://www.bibvip.com/en_US/";
+        // Navigate to the page.
+        driver.get(url);
+        // Create a thread pool executor.
+        ExecutorService threadPoolExecutor = Executors.newFixedThreadPool(10);
 
+// Submit the task to the thread pool executor.
+        threadPoolExecutor.submit(() -> {
+            try {
+                getAllLinksInPage(driver, url, "testlinks");
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+// Shutdown the thread pool executor when you are finished.
+        threadPoolExecutor.shutdown();
+
+// Wait for all of the tasks to finish executing.
+        threadPoolExecutor.awaitTermination(Long.MAX_VALUE, TimeUnit.MILLISECONDS);
 
     }
 
--- src/main/java/com/bibvip/utility/LinksRetriever.java
+++ src/main/java/com/bibvip/utility/LinksRetriever.java
@@ -1,13 +1,23 @@
 package com.bibvip.utility;
 
+import lombok.SneakyThrows;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import java.io.BufferedWriter;
 import java.io.FileWriter;
+
 import java.io.IOException;
 import java.io.Writer;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 /**
 This utility class is used for getting all the links in a page.
@@ -20,22 +30,60 @@ public class LinksRetriever {
         List<WebElement> links = driver.findElements(By.tagName("a"));
 
         // Create a CSV writer object.
-        Writer writer = new FileWriter(filename+".csv");
+        FileWriter writer = new FileWriter(filename+".csv");
+        BufferedWriter bufferedWriter = new BufferedWriter(writer);
 
         // Write the header row.
-        writer.write("Page - "+url+",Links\n");
+        bufferedWriter.write("Page - "+url+",Links,Status,Response Code\n");
+        bufferedWriter.flush();
 
         // Iterate through the link elements and write the link text and href to the CSV file.
         for (WebElement link : links) {
-            writer.write(url + "," + link.getAttribute("href") + "\n");
+            String href = link.getAttribute("href");
+            String httpCheck = "";
+            if(href == null){
+                httpCheck = "Link is null";
+            } else {
+                httpCheck = href.substring(0, 4);
+            }
+
+            String status = "";
+            int responseCode = 0;
+            if(httpCheck.startsWith("http")){
+                // Send a HTTP GET request to the URL specified in the href attribute
+                URL liveURL = new URL(href);
+                HttpURLConnection connection = (HttpURLConnection) liveURL.openConnection();
+                connection.setRequestMethod("GET");
+                connection.connect();
+
+                // Get the HTTP response code
+                responseCode = connection.getResponseCode();
+
+                // Check the HTTP response code to determine the status of the link
+
+                if (responseCode == 200) {
+                    status = "The link is OK.";
+                } else {
+                    status = "The link is broken.";
+                }
+
+                connection.disconnect();
+                bufferedWriter.write(url + "," + href + "," + status + "," + responseCode + "\n");
+                bufferedWriter.flush();
+            }
+            else {
+                status = "Not a valid link";
+                bufferedWriter.write(url + "," + href + "," + status + "," + responseCode + "\n");
+                bufferedWriter.flush();
+            }
+
         }
 
         // Close the CSV writer object.
-        writer.close();
+        bufferedWriter.close();
 
         // Quit the web driver.
         driver.quit();
     }
-
 
 }
